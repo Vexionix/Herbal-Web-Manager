b66f1895ff3525cd76f5b13abfafa358c77e17ad
Vexionix <djdarkstefan@gmail.com>
2024-06-20 16:57:22 +0300

    change error message

--- BackEnd/utils/errorHandlers.js
+++ BackEnd/utils/errorHandlers.js
@@ -1,5 +1,5 @@
 export const handleError = (error, res) => {
     console.error(error);
-    res.writeHead(500, { 'Content-Type': 'text/html' });
-    res.end('<h1>Internal Server Error</h1>');
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
 };
