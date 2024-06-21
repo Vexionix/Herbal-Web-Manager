3fd7ed803020c5107fdc2a3a04f25e7455759390
Vexionix <djdarkstefan@gmail.com>
2024-06-21 22:00:43 +0300

    add upload form handling (upload png or jpeg to local folder)

--- BackEnd/utils/router.js
+++ BackEnd/utils/router.js
@@ -6,6 +6,7 @@ import { handleNotFound } from '../routes/notFound.js';
 import { handlePlantApi } from './handlePlantApi.js';
 import { serveStaticFile } from './staticFileMiddleware.js';
 import { handleUnsplashRequest } from './unsplashHandler.js';
+import { handleFileUpload } from './fileUploadHandler.js';
 import url from 'url';
 
 const routes = {
@@ -26,10 +27,12 @@ const routes = {
         '/contact': handleContactForm,
         '/signup': handleSignupForm,
         '/login': handleLoginForm,
+        '/upload': handleFileUpload,
     }
 };
 
 export const router = async (req, res) => {
+    console.log(req.method, req.url, req.headers['content-type']);
     const { method } = req;
     const parsedUrl = url.parse(req.url);
     const pathname = parsedUrl.pathname;
