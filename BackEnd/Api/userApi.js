d9837192d9506c8f3e7bf24253f79d068a7bf115
Vexionix <djdarkstefan@gmail.com>
2024-06-22 23:38:06 +0300

    add new validation in add user

--- BackEnd/Api/userApi.js
+++ BackEnd/Api/userApi.js
@@ -8,6 +8,26 @@ export const handleUserAdd = async (req, res) => {
 
     req.on('end', async () => {
         const userData = JSON.parse(body);
+
+
+        if (userData.username === undefined || userData.firstName === undefined || userData.lastName === undefined || userData.password === undefined || userData.email === undefined) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Missing required fields' }));
+            return;
+        }
+
+        if (!/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/.test(userData.password)) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Password does not meet the criteria' }));
+            return;
+        }
+
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(userData.email)) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Invalid email' }));
+            return;
+        }
+
         const existingUser = await findUserByUsernameOrEmail(userData.username, userData.email);
         if (existingUser) {
             res.writeHead(409, { 'Content-Type': 'application/json' });
