be7cf40e794e6cd2409ae32700dcd1c7e3cdf0d1
Vexionix <djdarkstefan@gmail.com>
2024-06-24 13:16:27 +0300

    add home routing

---
+++ BackEnd/scripts/home.js
@@ -0,0 +1,27 @@
+function searchPlants() {
+    window.location.href = "/search";
+}
+
+function addPlants() {
+    //TODO
+}
+
+function myPlants() {
+    window.location.href = "/myPlants";
+}
+
+function myProfile() {
+    window.location.href = "/myProfile";
+}
+
+function likedImages() {
+    window.location.href = "/likedImages";
+}
+
+function topRedirect() {
+    window.location.href = "/top";
+}
+
+function unsplash() {
+    window.location.href = "/unsplash";
+}
