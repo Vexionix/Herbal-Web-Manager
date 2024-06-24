3137dfbe27c81926a998f88194d975d4c4b2b28d
Vexionix <djdarkstefan@gmail.com>
2024-06-24 13:14:41 +0300

    change catalog into top

---
+++ BackEnd/routes/top.js
@@ -0,0 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
+import { getContentType } from '../utils/contentTypes.js';
+import { handleError } from '../utils/errorHandlers.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontEndDirectory = path.join(__dirname, '..', "..", 'FrontEnd');
+
+export const handleTop = async (req, res) => {
+    try {
+        const filePath = path.join(frontEndDirectory, 'top.html');
+        const data = await fs.readFile(filePath);
+        const contentType = getContentType(filePath);
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.write(data);
+        res.end();
+    } catch (error) {
+        handleError(error, res);
+    }
+};
--- BackEnd/scripts/top.js
+++ BackEnd/scripts/top.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('/api/plants')
+    fetch('/api/plants/top')
         .then(response => response.json())
         .then(data => {
             const plantContainer = document.getElementById('plant-container');
@@ -34,41 +34,8 @@ function createPlantEntry(plant) {
 
     photoDiv.appendChild(img);
 
-    const likeDiv = document.createElement('div');
-    likeDiv.className = 'like';
-
-    const input = document.createElement('input');
-    input.type = 'checkbox';
-    input.id = `favoriteButton${plant.name}`;
-    input.className = 'favorite-button';
-
-    const label = document.createElement('label');
-    label.setAttribute('for', `favoriteButton${plant.name}`);
-    label.className = 'favorite-label';
-
-    const likeEmpty = document.createElement('span');
-    likeEmpty.className = 'like_empty';
-
-    const like0 = document.createElement('span');
-    like0.className = 'like0';
-    likeEmpty.appendChild(like0);
-
-    const likeFill = document.createElement('span');
-    likeFill.className = 'like_fill';
-
-    const like5 = document.createElement('span');
-    like5.className = 'like5';
-    likeFill.appendChild(like5);
-
-    label.appendChild(likeEmpty);
-    label.appendChild(likeFill);
-
-    likeDiv.appendChild(input);
-    likeDiv.appendChild(label);
-
     plantDiv.appendChild(infoDiv);
     plantDiv.appendChild(photoDiv);
-    plantDiv.appendChild(likeDiv);
 
     return plantDiv;
 }
